4e1e2c0a6763e186aa01979a517f1693766ab30a
edison-enriquez <62826864+edison-enriquez@users.noreply.github.com>
2020-04-02 21:09:21 -0500

    Create punto4.m

---
+++ punto4.m
@@ -0,0 +1,8 @@
+ s_char = convertStringsToChars(s)
+ str = convertCharsToStrings(A)
+ 
+ cadena="Todo lo que sucede una vez"
+letra : input('digite la letra que desea consultar: ','s');
+for i=1:length(cadena)
+    if 
+end
