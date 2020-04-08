8194ac90f0da8c5512c0a1355788c11ccf693358
edison-enriquez <62826864+edison-enriquez@users.noreply.github.com>
2020-04-08 14:05:26 -0500

    Update punto_2.m
    updated 8-04

--- punto_2.m
+++ punto_2.m
@@ -1,3 +1,19 @@
+%%
+% cargar datos respectivos al precio promedio del dolar en los últimos
+% 20 meses presentes en el archivo anexo dolar.txt. 
+%
+% load - Cargar variables del archivo en el espacio de trabajo
+%  sintaxis 
+%   load(filename) 
+%     - Si filenamees un archivo MAT, entonces load(filename)carga variables en el archivo MAT
+%     - Si filenamees un archivo ASCII, load(filename)crea una matriz de doble precisión que contiene datos del archivo
+%   load(filename,variables)
+%   Ver mas opciones (help load)
 
 x = load('dolar.txt') % cargamos el archivo dolar.txt
+
+% importdata - Cargar datos de un archivo
+%  Sintaxis
+%   A = importdata(filename) - carga datos en la matriz A.
+%%
 importdata('dolar.txt')
